b0b76dec1fe08e92b11af3f9305eb83165dee0f0
Simon Schindler <simon@alle-schindlers.de>
2023-12-16 19:25:36 +0100

    write docs

--- src/cnc/southbound/types.rs
+++ src/cnc/southbound/types.rs
@@ -1,3 +1,4 @@
+/// this is used to specify the yang-models that have to be loaded later.
 pub struct YangModule {
     pub name: &'static str,
     pub revision: Option<&'static str>,
@@ -5,6 +6,8 @@ pub struct YangModule {
 }
 
 impl YangModule {
+    /// this returnes a YangModule with only the name specified.
+    /// No specific revisions or features are loaded.
     pub const fn new(name: &'static str) -> Self {
         Self {
             name,
@@ -13,6 +16,8 @@ impl YangModule {
         }
     }
 
+    /// this returnes a YangModule with the name and features to load specified.
+    /// No specific revisions are loaded.
     pub const fn new_with_features(name: &'static str, features: &'static [&'static str]) -> Self {
         Self {
             name,
