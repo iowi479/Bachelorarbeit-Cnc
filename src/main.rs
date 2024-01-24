086e506b90848e1e7c8224d87f230b5fbe6e7d27
simon@alle-schindlers.de <simon@alle-schindlers.de>
2024-01-24 14:05:13 +0100

    redo example

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,20 @@ use ba::cnc::topology::MockTopology;
 use ba::cnc::Cnc;
 use std::sync::Arc;
 
+/// this is a example of calling the CNC with the specified components
 fn main() {
-    // Create needed Components
-    let northbound = MockUniAdapter::new(String::from("test-cuc-id"));
+    // configuration for CNC
+    let id: u32 = 0;
+    let domain: String = String::from("test-domain-id");
+
+    let cuc_id: String = String::from("test-cuc-id");
+
+    // create needed components
+    let northbound = MockUniAdapter::new(cuc_id);
     let southbound = NetconfAdapter::new();
     let storage = FileStorage::new();
     let topology = MockTopology::new_failing();
     let scheduler = MockTSNScheduler::new();
-
-    // Configuration for CNC
-    let id: u32 = 0;
-    let domain: String = String::from("test-domain-id");
 
     Cnc::run(
         id,
@@ -27,4 +30,7 @@ fn main() {
         Arc::new(topology),
         Arc::new(scheduler),
     );
+
+    // this is blocked until the cnc stops its operation
+    println!("CNC fully stopped and program is exiting...");
 }
